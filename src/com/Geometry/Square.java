2671bc2b5f93e29d01640593661391b4714b88ad
Amiel <s1920907@usls.edu.ph>
2021-04-24 15:10:21 +0800

    added Geometric abstract

---
+++ src/com/Geometry/Square.java
@@ -0,0 +1,16 @@
+package com.Geometry;
+
+public class Square extends GeometricFigure
+{
+	private double area;
+	
+	public Square(double height, double width, String figure)
+	{
+		super(height, width, figure);
+	}
+	public double Area(double height, double weight)
+	{
+			area = (height*weight);
+			return area;
+	}
+}
